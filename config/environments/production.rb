d0b5d360de6fd402718ab1916fb47b4b7fe3762e
panos <pstavrianosmail@gmail.com>
2018-10-12 01:17:39 +0300

    Init

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -104,4 +104,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  #devise
+  config.action_mailer.default_url_options = {:host => 'hairnflair.herokuapp.com'}
 end
